ccfaf2aa5adf372b296de6bd13a79513d105b446
Vseslav <vrashinskiyv@gmail.com>
2023-01-09 18:00:01 +0400

    feat: implement task hooks

--- my-app/src/components/Main/Main.tsx
+++ my-app/src/components/Main/Main.tsx
@@ -1,92 +1,122 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { SearchBar } from './Search';
+import STATE from '../constant/Cars';
 import CarCards from './CarsCard';
 import axios from 'axios';
 
-interface Data {
-  id: number;
-  name: string;
-  status: string;
-  species: string;
-  type: string;
-  gender: string;
-  origin: {
-    name: string;
-    url: string;
-  };
-  location: {
-    name: string;
-    url: string;
-  };
-  image: string;
-  episode: Array<string>;
-  url: string;
-  created: string;
+interface DataCarsProps {
+  cars: {
+    id: number;
+    car: string;
+    car_model: string;
+    color: string;
+    car_vin: string;
+    location: string;
+  }[];
 }
 
-const Main = () => {
-  const [results, setResults] = useState<Data[]>([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [isErr, setIsErr] = useState(false);
-  const [response, setResponse] = useState('https://rickandmortyapi.com/api/character');
+interface CarsState {
+  results: {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: {
+      name: string;
+      url: string;
+    };
+    location: {
+      name: string;
+      url: string;
+    };
+    image: string;
+    episode: Array<string>;
+    url: string;
+    created: string;
+  }[];
+  value: DataCarsProps;
+  searchTerm: string;
+  isLoaded: boolean;
+  isErr: boolean;
+  response: string;
+}
 
-  useEffect(() => {
-    async function componentDidMount() {
-      try {
-        const result = await axios.get(response);
-        setResults(result.data.results);
-        setIsLoaded(true);
-        setIsErr(false);
-      } catch (error) {
-        setIsLoaded(true);
-        setIsErr(true);
-      }
-      if (typeof localStorage.getItem('Term') === 'string') {
-        setSearchTerm(String(localStorage.getItem('Term')));
-      }
+export interface CarsProps {}
+
+class Main extends React.Component<CarsProps, CarsState> {
+  constructor(props: CarsProps) {
+    super(props);
+    this.state = {
+      results: [],
+      value: STATE,
+      searchTerm: '',
+      isLoaded: false,
+      isErr: false,
+      response: 'https://rickandmortyapi.com/api/character',
+    };
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+    this.handleSearchDataChange = this.handleSearchDataChange.bind(this);
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(this.state.response);
+      this.setState({ results: response.data.results, isLoaded: true, isErr: false });
+    } catch (error) {
+      this.setState({ isLoaded: true, isErr: true });
     }
-    componentDidMount();
-  }, [response]);
+    if (typeof localStorage.getItem('Term') === 'string') {
+      this.setState({ searchTerm: String(localStorage.getItem('Term')) });
+    }
+  }
 
-  useEffect(() => {
-    setIsLoaded(false);
-  }, [response]);
+  componentDidUpdate(prevProps: Readonly<CarsProps>, prevState: Readonly<CarsState>): void {
+    if (prevState.response !== this.state.response) {
+      this.setState({ response: this.state.response, isLoaded: false });
+      this.componentDidMount();
+    }
+  }
 
-  const handleSearchTermChange = (searchTerm: string) => {
+  handleSearchTermChange = (searchTerm: string) => {
     localStorage.setItem('Term', searchTerm);
-    localStorage.setItem('Response', response + '/?name=' + searchTerm);
-    setSearchTerm(searchTerm);
+    localStorage.setItem('Response', this.state.response + '/?name=' + searchTerm);
+    this.setState({ searchTerm });
   };
 
-  const handleSearchDataChange = () => {
+  handleSearchDataChange = () => {
     if (typeof localStorage.getItem('Response') === 'string') {
-      setResponse(
-        'https://rickandmortyapi.com/api/character' + '/?name=' + localStorage.getItem('Term')
-      );
+      this.setState({
+        response:
+          'https://rickandmortyapi.com/api/character' + '/?name=' + localStorage.getItem('Term'),
+      });
     }
   };
 
-  return (
-    <div className="main">
-      <SearchBar
-        searchTerm={searchTerm}
-        handleSearchTermChange={handleSearchTermChange}
-        handleSearchDataChange={handleSearchDataChange}
-      />
-      <div className="cards">
-        {!isLoaded ? (
-          <h1>
-            Loading <img height={22} src="https://i.ibb.co/RpSP280/6.gif"></img>
-          </h1>
-        ) : (
-          (!isErr && results.map((item) => <CarCards key={item.id} item={item} />)) || (
-            <h1>Not Found</h1>
-          )
-        )}
+  render() {
+    return (
+      <div className="main">
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          handleSearchTermChange={this.handleSearchTermChange}
+          handleSearchDataChange={this.handleSearchDataChange}
+        />
+        <div className="cards">
+          {!this.state.isLoaded ? (
+            <h1>
+              Loading <img height={22} src="https://i.ibb.co/RpSP280/6.gif"></img>
+            </h1>
+          ) : (
+            (!this.state.isErr &&
+              this.state.results.map((item) => <CarCards key={item.id} item={item} />)) || (
+              <h1>Not Found</h1>
+            )
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Main;
